823bf05327efebfb34e4bfc757d10ea62783943b
Anderson Leonardo <anderson.ldsilv@gmail.com>
2023-08-18 14:15:19 -0300

    Single File components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header">
+        header
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+
+    }
+</script>
+
+<style>
+    .header{
+        background-color: #000;
+        color: #fff;
+    }
+</style>
